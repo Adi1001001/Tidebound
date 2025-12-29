6768b63da0950f16f2936613950c4dd68e2a40f8
Adi1001001 <adi.padhi2010@gmail.com>
2025-12-29 17:03:18 +1100

    Added gizmos lines to visualise the boundaries.

--- Assets/Scripts/Boundaries.cs
+++ Assets/Scripts/Boundaries.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class Boundaries : MonoBehaviour {
     public float currentCentre = 0f;
     public float targetCentre = 0f;
-    public float currentWidth = 5f;
-    public float shiftAmount = 1.5f; // How far it can move left/right per update (rn its one second)
-    public float smoothSpeed = 2f; // How quickly the boundary smoothly moves to the target position
+    public float currentWidth = 15f;
+    public float shiftAmount = 2f; // How far it can move left/right per update (rn its one second)
+    public float smoothSpeed = 3f; // How quickly the boundary smoothly moves to the target position
     
     void Start() {
         InvokeRepeating("CalculateNewPath", 1f, 1f); // Runs every 1 second
@@ -13,12 +13,23 @@ public class Boundaries : MonoBehaviour {
 
     void CalculateNewPath() {
         targetCentre = Random.Range(-shiftAmount, shiftAmount);
-        targetCentre = Mathf.Clamp(targetCentre, -2.5f, 2.5f); // Keeping the center within a certain range so it doesn't leave the screen
-        Debug.Log("River center updated to: " + currentCentre + ", width: " + currentWidth);  
+        // targetCentre = Mathf.Clamp(targetCentre, -5f, 5f); // Keeping the centre within a certain range so it doesn't leave the screen
+        Debug.Log("River center updated to: " + targetCentre + ", width: " + currentWidth);  
     }
 
     void Update() {
         currentCentre = Mathf.Lerp(currentCentre, targetCentre, Time.deltaTime * smoothSpeed);
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        // Drawing the left edge
+        Vector3 left = new Vector3(currentCentre - (currentWidth / 2), 0, 0);
+        Gizmos.DrawRay(left + Vector3.up * 10, Vector3.down * 20);
+
+        // Drawing the right edge
+        Vector3 right = new Vector3(currentCentre + (currentWidth / 2), 0, 0);
+        Gizmos.DrawRay(right + Vector3.up * 10, Vector3.down * 20);
     }
 }
 // this script works in real time, and updates the boundary when the player is already there.
