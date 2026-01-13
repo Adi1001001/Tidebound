cd314ff7b35b2f537fa82220319504d581dbed36
Adi1001001 <adi.padhi2010@gmail.com>
2026-01-13 12:02:00 +1100

    Optimised the current speed logic.
    Going to fix the camera movements now and later i am going to add a highest speed stat.

--- Assets/Scripts/Currents.cs
+++ Assets/Scripts/Currents.cs
@@ -5,18 +5,32 @@ public class Currents : MonoBehaviour {
     private Rigidbody2D playerRb;
     private CameraController cameraController;
     private PlayerController playerController;
-    private void OnTriggerStay2D(Collider2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
             Debug.Log("Player has entered a current!");
             playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
             cameraController = FindFirstObjectByType<CameraController>();
-            playerController = FindFirstObjectByType<PlayerController>();
-            playerController.inCurrent = true;
-
+            playerController = collision.gameObject.GetComponent<PlayerController>();
+            
+            if (playerController != null) playerController.inCurrent = true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
             if (playerRb != null) {
                 playerRb.AddForce(transform.up * pushForce); // you have to adjust the direction of the current in unity
                 cameraController.CurrentExtraLead();
             }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Debug.Log("Player has exited a current!");
+            if (playerController != null) playerController.inCurrent = false;
+            // clear references so we don't accidentally try to push a player who left the current
+            playerRb = null;
+            cameraController = null;
+            playerController = null;
         }
     }
 }
