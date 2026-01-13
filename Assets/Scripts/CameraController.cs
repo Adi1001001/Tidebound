92f83fa514fc6e98d3f3d863c4d7b9f37fa9207a
Adi1001001 <adi.padhi2010@gmail.com>
2026-01-13 12:13:54 +1100

    Saving ...

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,31 +2,35 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Transform target; // The player transform
-    private Rigidbody2D targetRb;
-    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f); // Offset from the target
+    private Transform playerTransform;
+    private Rigidbody2D playerRb;
+    private PlayerController playerController;
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f); // Offset from the playerTransform
     public float leadAmount = 1.5f;
     public float smoothSpeed = 5.0f;
 
     void Start() {
-        if (target == null) {
-            target = FindFirstObjectByType<PlayerController>().transform; // finding the player
-            targetRb = target.GetComponent<Rigidbody2D>();
+        if (playerTransform == null) {
+            playerController = FindFirstObjectByType<PlayerController>();
+            playerTransform = playerController.transform; // finding the player
+            playerRb = playerTransform.GetComponent<Rigidbody2D>();
         }
     }
 
     void LateUpdate() { // this function happens after all the other update calls
-        if (target == null) return; // fallback if player is still not assigned
+        if (playerTransform == null) return; // fallback if player is still not assigned
 
-        Vector3 desiredPosition = target.position + offset;
+        if (playerController.inCurrent) return; // do not update position if in current, handled by CurrentExtraLead()
+
+        Vector3 desiredPosition = playerTransform.position + offset;
         transform.position = desiredPosition;
     }
 
     public void CurrentExtraLead() {
         Debug.Log("Current extra lead activated");
-        Vector3 leadOffset = targetRb.linearVelocity.normalized * leadAmount;
-        Vector3 targetPosition = target.position + offset + leadOffset;
+        Vector3 leadOffset = playerRb.linearVelocity.normalized * leadAmount;
+        Vector3 playerTransformPosition = playerTransform.position + offset + leadOffset;
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, playerTransformPosition, smoothSpeed * Time.deltaTime);
     }
 }
