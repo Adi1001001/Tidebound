fcb9a25c3c3935554e633acb19da9b6ac6b939af
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2026-04-18 13:32:09 +1000

    Imported map 1 and made slight changes to timers

---
+++ Assets/com.seanba.super-tiled2unity/FINAL/Tilesets/TSXs/Grass.tsx
@@ -0,0 +1,419 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Grass" tilewidth="32" tileheight="32" tilecount="48" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="96">
+  <image source="../PNGs/Grass Map Tiles/1.1.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="6">
+    <polygon points="0,0 -23,0 -26,3.04348 -26,26 -6,25.9565 -6.04348,21.0435 -4.95652,21 -4.95652,19.9565 0,19.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <image source="../PNGs/Grass Map Tiles/1.2.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="5.95652" width="31.913" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <image source="../PNGs/Grass Map Tiles/1.3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="26">
+    <polygon points="0,0 5.08696,0 5.04348,0.913043 5.95652,0.913043 6,5.91304 32.087,6 32.0435,-20.0435 0.0434783,-19.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <image source="../PNGs/Grass Map Tiles/1.4.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6.08696">
+    <polygon points="0,0 23,-0.0869565 26.0435,2.91304 26.087,25.9565 0.0434783,25.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <image source="../PNGs/Grass Map Tiles/1.5.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="31.913">
+    <polygon points="0,0 -0.0434783,-22.8696 3,-26 26.087,-25.9565 26.0435,0.0869565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <image source="../PNGs/Grass Map Tiles/1.6.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="5.95652" width="31.8696" height="26.0435"/>
+  </objectgroup>
+ </tile>
+ <tile id="102">
+  <image source="../PNGs/Grass Map Tiles/1.7.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="6">
+    <polygon points="0,0 -32.0435,0.0434783 -32.0435,25.9565 -5.91304,26 -5.95652,21 -4.91304,21.087 -4.95652,19.9565 0,19.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <image source="../PNGs/Grass Map Tiles/1.8.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="6.04348">
+    <polygon points="0,0 23,-0.0869565 26,3 26,25.8696 0,25.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <image source="../PNGs/Grass Map Tiles/2.1.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="-0.0434783">
+    <polygon points="0,0 -0.0869565,32 20.0435,32.0435 20.0435,27.087 21.0435,27.087 20.9565,26.087 26.0435,26.0435 26,6.04348 21.0435,6.08696 21.0435,5.08696 20,5.08696 20.0435,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <image source="../PNGs/Grass Map Tiles/2.2.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6">
+    <polygon points="0,0 32,0.0434783 32,20 27,20 27.0435,20.9565 26.087,21 26.087,25.913 6.04348,25.9565 6.04348,21 5.04348,21.0435 5.04348,20.0435 0,19.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <image source="../PNGs/Grass Map Tiles/2.3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6">
+    <polygon points="0,0 5,0.0434783 5.04348,-1.04348 6.04348,-1.04348 6,-6.04348 32.087,-6 32,19.9565 27.0435,19.9565 27.0435,21.0435 26,21.0435 26,25.9565 5.95652,26 6,21 5.04348,21 5.04348,20 0.0434783,19.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <image source="../PNGs/Grass Map Tiles/2.4.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="26">
+    <polygon points="0,0 5.04348,-0.0434783 5.04348,0.956522 6.04348,0.956522 6.04348,6.04348 32.1304,6 32.087,-20.0435 27,-19.9565 27.087,-21 26.0435,-21.0435 26.087,-26.0435 0,-26"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <image source="../PNGs/Grass Map Tiles/2.5.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0869565" y="6">
+    <polygon points="0,0 5.13043,-0.0434783 5.04348,-1 6.08696,-1 6.08696,-6.04348 32.1739,-6 32.1304,25.8696 0.0869565,25.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <image source="../PNGs/Grass Map Tiles/2.6.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0869565" width="32.0435" height="32.0435"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <image source="../PNGs/Grass Map Tiles/2.7.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0869565" width="26" height="32.0435"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <image source="../PNGs/Grass Map Tiles/2.8.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="-0.0434783" width="19.9565" height="32.087"/>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <image source="../PNGs/Grass Map Tiles/3.1.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="0.0434783">
+    <polygon points="0,0 0.0434783,32 26.0435,31.9565 26,5.95652 21,6 21,5.04348 19.9565,5 19.913,-0.173913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <image source="../PNGs/Grass Map Tiles/3.2.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6.04348">
+    <polygon points="0,0 5.04348,0 5.08696,-1.08696 6,-1.08696 6.08696,-6.08696 26,-6.08696 26.0435,-1.13043 27,-1.08696 27,0 32.087,-0.0434783 32,19.8696 27,19.913 27.087,20.913 26,20.8696 26.0435,25.9565 0.0869565,25.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <image source="../PNGs/Grass Map Tiles/3.3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6">
+    <polygon points="0,0 5.13043,0.0434783 5.13043,-1 6.08696,-1 6.08696,-6.04348 26.1304,-5.95652 26,-1.08696 27.087,-1.04348 27.0435,-0.0434783 32.087,0 32.0435,20 27.087,19.8696 27,21 26,20.9565 26.087,25.913 6,25.913 6,21 5,21 5,20 0.0869565,20"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="67">
+  <image source="../PNGs/Grass Map Tiles/3.4.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="5.95652">
+    <polygon points="0,0 4.95652,0.0434783 5,-0.956522 6,-0.956522 5.95652,-6.04348 32.0435,-5.95652 32,26 5.95652,26.0435 6,21.0435 5,21.087 5,20.0435 -0.0434783,20"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <image source="../PNGs/Grass Map Tiles/3.5.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25.913" y="31.9565">
+    <polygon points="0,0 -0.0434783,-5 1.08696,-5 1.04348,-6 6.04348,-6 6.08696,-32.0435 -26,-32 -25.9565,-0.0869565 -25.913,0.0869565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <image source="../PNGs/Grass Map Tiles/3.6.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="26">
+    <polygon points="0,0 5.04348,-0.0434783 5,1.04348 6,1.04348 6,6 26,6 26,1.04348 27.1304,1.04348 27.0435,0 32.0435,-0.0434783 32.0435,-26.087 -0.0869565,-26"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <image source="../PNGs/Grass Map Tiles/3.7.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="26.0435" y="0.0434783">
+    <polygon points="0,0 -0.0434783,31.8696 -20.087,31.913 -20.0435,26.9565 -21,26.9565 -21.087,25.9565 -26,25.913 -26.1304,-0.130435"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="119">
+  <image source="../PNGs/Grass Map Tiles/3.8.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.91304" y="-0.0434783">
+    <polygon points="0,0 0.0869565,23 3.04348,25.9565 17,26 20.1304,23 20.0435,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <image source="../PNGs/Grass Map Tiles/4.1.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="32" y="25.913">
+    <polygon points="0,0 -5.04348,0.0434783 -5,1.13043 -6.04348,1.04348 -6.08696,6.08696 -26.0435,6.08696 -25.9565,-26.087 -0.0869565,-25.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="73">
+  <image source="../PNGs/Grass Map Tiles/4.2.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0869565" y="26">
+    <polygon points="0,0 5.08696,-0.0434783 5.08696,0.956522 6.04348,0.956522 6.08696,5.95652 26.087,5.95652 26.1304,1 27.087,1 27.087,-0.0434783 32.1304,0 32.087,-20.0435 27.1304,-20 27.087,-20.9565 26.1304,-20.9565 26.0435,-25.9565 0.0434783,-26"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="74">
+  <image source="../PNGs/Grass Map Tiles/4.3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="26" y="32">
+    <polygon points="0,0 -0.0869565,-32.1304 -20.0435,-32 -19.9565,-27 -21,-27 -21,-26 -26,-26 -26.0435,-6 -20.9565,-6 -21,-5.08696 -19.9565,-5.08696 -20,-0.130435"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="75">
+  <image source="../PNGs/Grass Map Tiles/4.4.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="-0.0869565" width="26" height="32.087"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <image source="../PNGs/Grass Map Tiles/4.5.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="25.9565" y="0">
+    <polygon points="0,0 0,5 1.08696,4.95652 1,6 6.04348,6 6.04348,31.9565 -25.9565,32 -25.9565,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <image source="../PNGs/Grass Map Tiles/4.6.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="6.04348">
+    <polygon points="0,0 5.08696,0 5,-1.08696 6.04348,-1.08696 6.04348,-6.08696 26,-6.08696 25.9565,-1.17391 26.9565,-1.08696 27,0.0434783 31.9565,0 31.9565,25.8696 -0.130435,25.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="78">
+  <image source="../PNGs/Grass Map Tiles/4.7.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="26" y="32.0435">
+    <polygon points="0,0 0.0434783,-32.1304 -20.087,-32.0435 -20,-27.1304 -21.0435,-27.1304 -21.0435,-26.0435 -26,-26.087 -26,-0.0869565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="120">
+  <image source="../PNGs/Grass Map Tiles/4.8.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.95652" y="32.0435">
+    <polygon points="0,0 0.0434783,-23.087 3.04348,-26.087 17,-26.087 20,-23.0435 19.9565,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="110">
+  <image source="../PNGs/Grass Map Tiles/5.1.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="0">
+    <polygon points="0,0 0,22.8696 3,25.8696 26.0435,26 25.913,5.95652 20.9565,6 21,5.13043 20,5 19.913,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <image source="../PNGs/Grass Map Tiles/5.2.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6">
+    <polygon points="0,0 5.08696,0.0434783 5.08696,-1 6.04348,-1 6.04348,-6.04348 26.0435,-6.04348 26.0435,-1.13043 27.0435,-1.13043 27.0435,0 32,0 32.0435,19.913 0.0434783,19.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <image source="../PNGs/Grass Map Tiles/5.3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6">
+    <polygon points="0,0 5,0.0434783 5.04348,-1.04348 6.04348,-1.04348 6.08696,-6 26.087,-6 26.0435,-1 26.9565,-1 27.0435,-0.0434783 32.2174,0 32.1304,25.913 6.04348,25.913 6,21 5.08696,21.0435 5.08696,19.913 -0.0434783,19.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <image source="../PNGs/Grass Map Tiles/5.4.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="6">
+    <polygon points="0,0 4.95652,0.0434783 5.04348,-1.08696 6.08696,-1.04348 6,-6.13043 32,-6.13043 31.9565,19.913 26.913,19.9565 27,20.9565 25.913,20.9565 26,26 0.0869565,25.913 -0.0434783,25.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <image source="../PNGs/Grass Map Tiles/5.5.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="25.913">
+    <polygon points="0,0 5.04348,0.0434783 5.04348,1.04348 6,1.04348 6.04348,6.08696 32.0435,6.08696 32.087,-25.9565 0.0434783,-25.913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <image source="../PNGs/Grass Map Tiles/5.6.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0869565" width="32" height="32.0435"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <image source="../PNGs/Grass Map Tiles/5.7.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25.913" y="0.0434783">
+    <polygon points="0,0 0.0434783,5 1.08696,4.95652 0.956522,5.95652 6.04348,5.95652 6.04348,25.8696 1.04348,25.9565 1.04348,27 0,26.9565 0,31.913 -25.913,31.913 -25.913,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <image source="../PNGs/Grass Map Tiles/5.8.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6">
+    <polygon points="0,0 5.08696,0.0434783 5.13043,-0.956522 6.13043,-0.956522 6.04348,-6.13043 26.0435,-6 26.0435,16.8696 23.0435,19.9565 0.0434783,20"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <image source="../PNGs/Grass Map Tiles/6.1.png" width="32" height="32"/>
+ </tile>
+ <tile id="121">
+  <image source="../PNGs/Grass Map Tiles/6.2.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31.913" y="5.95652">
+    <polygon points="0,0 -22.8696,0.0434783 -25.9565,3.04348 -25.9565,17 -22.913,19.9565 0.130435,20"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="113">
+  <image source="../PNGs/Grass Map Tiles/6.3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="25.9565">
+    <polygon points="0,0 32,0 32.087,-26 6,-26 5.95652,-21 5.04348,-21 5,-19.9565 -0.0434783,-20"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="114">
+  <image source="../PNGs/Grass Map Tiles/6.4.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.130435" y="25.9565">
+    <polygon points="0,0 23.1739,0.0434783 26.1304,-3.04348 26.0435,-26.087 0.130435,-26"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="115">
+  <image source="../PNGs/Grass Map Tiles/6.5.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="0">
+    <polygon points="0,0 -0.0434783,23.0435 3,25.8696 26,26.0435 25.9565,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <image source="../PNGs/Grass Map Tiles/6.6.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0434783" width="31.913" height="26.0435"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <image source="../PNGs/Grass Map Tiles/6.7.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25.8696" y="0.0434783">
+    <polygon points="0,0 0.0869565,5.04348 1.04348,5.04348 1.08696,5.95652 6.17391,6 6.13043,25.9565 -25.9565,25.913 -25.913,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="122">
+  <image source="../PNGs/Grass Map Tiles/6.8.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="6">
+    <polygon points="0,0 23,0.0434783 26,3.08696 26.0435,17.087 23.0435,19.9565 0,19.9565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Land" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="57" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="58" wangid="1,1,1,0,1,0,1,0"/>
+   <wangtile tileid="59" wangid="1,0,1,1,1,0,1,1"/>
+   <wangtile tileid="60" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="61" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="62" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="65" wangid="1,0,1,0,1,1,1,0"/>
+   <wangtile tileid="66" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="67" wangid="1,1,1,1,1,0,1,0"/>
+   <wangtile tileid="68" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="69" wangid="1,1,1,0,1,0,1,1"/>
+   <wangtile tileid="70" wangid="1,0,0,0,1,0,1,1"/>
+   <wangtile tileid="73" wangid="1,0,1,0,1,0,1,1"/>
+   <wangtile tileid="74" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="75" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="76" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="77" wangid="1,0,1,1,1,1,1,0"/>
+   <wangtile tileid="78" wangid="1,0,0,0,1,1,1,0"/>
+   <wangtile tileid="81" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="82" wangid="1,0,1,1,1,0,1,0"/>
+   <wangtile tileid="83" wangid="1,1,1,0,1,1,1,0"/>
+   <wangtile tileid="84" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="85" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="86" wangid="1,0,1,0,1,1,1,1"/>
+   <wangtile tileid="96" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="97" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="98" wangid="0,0,1,1,1,0,1,0"/>
+   <wangtile tileid="99" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="100" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="101" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="102" wangid="0,0,1,0,1,1,1,0"/>
+   <wangtile tileid="103" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="104" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="105" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="106" wangid="1,0,1,1,1,0,0,0"/>
+   <wangtile tileid="108" wangid="1,1,1,0,1,0,0,0"/>
+   <wangtile tileid="110" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="111" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="113" wangid="1,1,1,0,0,0,1,0"/>
+   <wangtile tileid="114" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="115" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="116" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="117" wangid="1,0,1,0,0,0,1,1"/>
+   <wangtile tileid="119" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="120" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="121" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="122" wangid="0,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
