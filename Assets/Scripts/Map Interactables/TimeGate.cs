a3a52062ba1db065b029521021660624c29dce61
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2026-08-16 19:53:00 +1000

    Added time zone animation

--- Assets/Scripts/Map Interactables/TimeGate.cs
+++ Assets/Scripts/Map Interactables/TimeGate.cs
@@ -4,11 +4,13 @@ public class TimeGate : MonoBehaviour
 {
     public int extraTime = 5;
     private bool claimedTime = false;
+    private GameObject checkpointNum;
     TimerManager timerManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timerManager = FindAnyObjectByType<TimerManager>();
+        checkpointNum = transform.Find("Checkpoint Num").gameObject;
     }
 
     // Update is called once per frame
@@ -22,6 +24,8 @@ public class TimeGate : MonoBehaviour
         if (!claimedTime)
         {
             timerManager.AddTime(extraTime);
+            GetComponent<SpriteRenderer>().color = Color.grey;
+            checkpointNum.GetComponent<SpriteRenderer>().color = Color.white;
             claimedTime = true;
         }
     }
