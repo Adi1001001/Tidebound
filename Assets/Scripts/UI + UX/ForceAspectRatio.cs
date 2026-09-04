bc24b2e07ec1ef325f8c82c3880de4d51d1118e6
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2026-09-04 20:16:01 +1000

    Fixed button bug

--- Assets/Scripts/UI + UX/ForceAspectRatio.cs
+++ Assets/Scripts/UI + UX/ForceAspectRatio.cs
@@ -1,38 +1,50 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class ForceAspectRatio : MonoBehaviour
 {
-    void Start()
+    private const float TargetAspect = 16f / 9f;
+
+    private Camera cam;
+
+    private int lastScreenWidth;
+    private int lastScreenHeight;
+
+    private void Start()
     {
-        float targetAspect = 16.0f / 9.0f;
-        float windowAspect = (float)Screen.width / (float)Screen.height;
-        float scaleHeight = windowAspect / targetAspect;
+        cam = GetComponent<Camera>();
 
-        Camera camera = GetComponent<Camera>();
+        UpdateAspectRatio();
 
-        if (scaleHeight < 1.0f)
-        {  
-            Rect rect = camera.rect;
+        lastScreenWidth = Screen.width;
+        lastScreenHeight = Screen.height;
+    }
 
-            rect.width = 1.0f;
-            rect.height = scaleHeight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleHeight) / 2.0f;
-            
-            camera.rect = rect;
-        }
-        else 
+    private void Update()
+    {
+        if (Screen.width != lastScreenWidth ||
+            Screen.height != lastScreenHeight)
         {
-            float scaleWidth = 1.0f / scaleHeight;
+            UpdateAspectRatio();
 
-            Rect rect = camera.rect;
+            lastScreenWidth = Screen.width;
+            lastScreenHeight = Screen.height;
+        }
+    }
 
-            rect.width = scaleWidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scaleWidth) / 2.0f;
-            rect.y = 0;
+    private void UpdateAspectRatio()
+    {
+        float windowAspect = (float)Screen.width / Screen.height;
+        float scaleHeight = windowAspect / TargetAspect;
 
-            camera.rect = rect;
+        if (scaleHeight < 1f)
+        {
+            cam.rect = new Rect(0f, (1f - scaleHeight) / 2f, 1f, scaleHeight);
+        }
+        else
+        {
+            float scaleWidth = 1f / scaleHeight;
+            cam.rect = new Rect((1f - scaleWidth) / 2f, 0f, scaleWidth, 1f);
         }
     }
 }
